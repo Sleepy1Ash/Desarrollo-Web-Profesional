e7b8718174059a497178766c23c773dc1f9c2189
Sleepy1Ash <71827180+Sleepy1Ash@users.noreply.github.com>
2025-01-18 16:49:24 -0600

    Delete if_conditional.py

--- if_conditional.py
+++
@@ -1,6 +0,0 @@
-# if conditional statments
-
-age =int(input('¿Cual es tu edad? '))
-if age >= 18:
-    print(f'Con tu edad actuad: {age}, ya eres  mayor de edad.')
-    print('Hola cadena adicional')

2484e8d43a76746590310c3426ec1219362c856f
Sleepy1Ash <71827180+Sleepy1Ash@users.noreply.github.com>
2025-01-18 13:08:13 -0600

    Add files via upload

---
+++ assignaments.py
@@ -0,0 +1,12 @@
+#Forma simple o unitaria
+message = 'Hola Mundo'
+
+#Asignacion multiple
+x,y,z = 10, 6.5, 'Jorge'
+
+#Asignacion en cadena
+a = b = c = 35
+
+print("Valro del mensaje: ", message)
+print(x,y,z)
+print(a,b,c)
